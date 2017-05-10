ba1d499e05aaf047cd456f3a21e470399be58cdf
dmitriys09 <dima972005@gmail.com>
2017-05-10 16:18:56 +0700

    покрыл тестами проверку корректности входных данных

--- test/deposit_test.c
+++ test/deposit_test.c
@@ -8,6 +8,26 @@ CTEST(check, test1)
 {
 	ASSERT_EQUAL(0, check(365, 10000));
 }
+CTEST(check, test2)
+{
+	ASSERT_EQUAL(-1, check(366, 10000));
+}
+CTEST(check, test3)
+{
+	ASSERT_EQUAL(-1, check(365, 9999));
+}
+CTEST(check, test4)
+{
+	ASSERT_EQUAL(-1, check(366, 9999));
+}
+CTEST(check, test5)
+{
+	ASSERT_EQUAL(-1, check(-10, 10000));
+}
+CTEST(check, test6)
+{
+	ASSERT_EQUAL(-1, check(365, -10000));
+}
 
 
 
